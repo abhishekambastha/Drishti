674a4d05a3d1d88097c53b612a81ad360e0c30db
Abhishek Ambastha <ambastha.abhishek@gmail.com>
2014-11-03 18:08:11 +0000

    Mac Commit

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import numpy as np
-import PIL.Image as Image
+import cv2
 import os, sys
+import PIL.Image as Image
 
-for dirname, dirnames, filenames in os.walk("/home/abhishek/faces"):
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/faces"):
 	print filenames
 
+print dirname
 X,y = [],[]
 c=1
 for name in filenames:
-	s = "" + dirname +"/" + name
+	s = dirname +"/" + name
 	im = Image.open(s)
 	im.convert("L")
 	X.append(np.asarray(im, dtype=np.uint8))
