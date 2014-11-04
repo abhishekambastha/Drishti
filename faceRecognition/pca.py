50ee7059732d86897aea2f5fa89a110cc8ca1664
Abhishek Ambastha <ambastha.abhishek@gmail.com>
2014-11-04 20:56:49 +0000

    Face detection works for 4

--- faceRecognition/pca.py
+++ faceRecognition/pca.py
@@ -61,5 +61,157 @@ x = np.dot(A.T,eVect)
 print x.shape
 
 #create model for person 1,2,3......................
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p1"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P1,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P1.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model1 = asColumnMatrix(P1)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p2"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+
+## Populate the List X with 2D image arrays
+P2,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P2.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model2 = asColumnMatrix(P2)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p3"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P3,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P3.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model3 = asColumnMatrix(P3)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p4"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P4,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P4.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model4 = asColumnMatrix(P4)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p5"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P5,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P5.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model5 = asColumnMatrix(P5)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p6"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P6,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P6.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+model6 = asColumnMatrix(P6)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p7"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P7,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P7.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+model7 = asColumnMatrix(P7)
+
+for dirname, dirnames, filenames in os.walk("/Users/abhi/projDrishti/trainFaces/p8"):
+	a=1;
+
+print "Face Database Location ",dirname
+
+
+## Populate the List X with 2D image arrays
+P8,y = [],[]
+c=0
+for name in filenames:
+	s = dirname +"/" + name
+	im = Image.open(s)
+	im.convert("L")
+	P8.append(np.asarray(im, dtype=np.uint8))
+	y.append(c)
+	c = c+1
+
+
+model8 = asColumnMatrix(P8)
 
 #query image
