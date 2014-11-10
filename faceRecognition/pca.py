d77ab9c13641b5b19631d6a59e1566d173ae2ff2
Abhishek Ambastha <ambastha.abhishek@gmail.com>
2014-11-10 22:46:05 +0000

    Moved generateModel method from getFiles to PCA.py

--- faceRecognition/pca.py
+++ faceRecognition/pca.py
@@ -7,3 +7,20 @@ def PCA(A):
 	index = np.argsort(-eVal)
 	eVect = eVect[:,index]
 	return eVal, eVect
+
+def generateModels(modelList, eVect):
+	
+	modelParameters = []
+	
+	#confusion in maths here
+
+	print "evect", eVect.shape
+	print "imageList", modelList[0].T.shape
+	for i in range(len(modelList)):
+		modelParameters.append(np.dot(modelList[i].T, eVect))
+
+	print len(modelParameters)
+	print modelParameters[0].shape   # contains a for an image in a row, corresponding to 0 model
+	return modelParameters  #return coefficients obtained!
+
+
